063c9ddd58408f3af9954be5cee6f7f7ca27d50c
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-12 16:31:04 -0400

    initial commit

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Eric', age: 20, absent: true},
+  {name: 'Claudia', age: 21, absent: false},
+  {name: 'Tom', age: 19, absent: false},
+]
+
+export { 
+	students
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+// import modules
+import express from 'express'
+// import students data
+import { students } from './data/students-data.js'
+
+// create express app
+const app = express()
+
+// configure the app (app.set)
+app.set('view engine', 'ejs')
+
+// mount Middleware (app.use)
+
+// mount routes
+app.get('/', function(req, res) {
+  res.redirect('/home')
+})
+
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
+app.get('/students', function(req, res) {
+  res.render('index', {
+    students: students
+  })
+})
+
+// tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
